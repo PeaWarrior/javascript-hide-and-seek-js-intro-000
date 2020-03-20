744c214c4e72cbe1efa4f8baf30feb7f4adffc6b
Jackson Chen <jacksonchen171@gmail.com>
2020-03-20 22:30:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,8 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i] += n 
+  }
 }
