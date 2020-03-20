79e6f69161cd8c694bee9e4fab99894a0d806cac
Jackson Chen <jacksonchen171@gmail.com>
2020-03-20 23:20:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function increaseRankBy(n) {
     lis[i].innerHTML = (parseInt(lis[i].innerHTML, 10) + n).toString()
   }
 }
+
+function deepestChild() {
+  let current = document.getElementById('grand-node').querySelectorAll('div')
+  return current
+  
+}
